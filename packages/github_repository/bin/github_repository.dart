a3d5200d9fe7867f81e8e761b915c5ee9465041b
hydev777 <hytechwilson@gmail.com>
2024-02-03 10:25:49 -0400

    feat(api_client): setting up client

---
+++ packages/github_repository/bin/github_repository.dart
@@ -0,0 +1,5 @@
+import 'package:github_repository/github_repository.dart' as github_repository;
+
+void main(List<String> arguments) {
+  print('Hello world: ${github_repository.calculate()}!');
+}
---
+++ packages/github_repository/test/github_repository_test.dart
@@ -0,0 +1,8 @@
+import 'package:github_repository/github_repository.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
