4d229eb751b1bc07456f3978afbf706575e46dae
hydev777 <hytechwilson@gmail.com>
2024-02-04 09:48:22 -0400

    feat(github_repository): completed testing and implementation

--- packages/github_repository/lib/src/github_repository.dart
+++ packages/github_repository/lib/src/github_repository.dart
@@ -1,1 +1,50 @@
-class GithubRepository {}
+import 'dart:convert';
+
+import 'package:github_api/github_api.dart';
+import 'package:http/http.dart' as http;
+
+import 'models/commit.dart';
+
+class HttpException implements Exception {}
+
+class JsonDecodeException implements Exception {}
+
+class HttpRequestFailure implements Exception {
+  const HttpRequestFailure(this.statusCode);
+
+  final int statusCode;
+}
+
+class GithubRepository {
+  GithubRepository({
+    required GithubApi githubApi,
+  }) : _githubApi = githubApi;
+
+  final GithubApi _githubApi;
+
+  Future<List<Commit>> fetchListOfCommitsInRepo(
+      String owner, String repository) async {
+    http.Response response;
+    try {
+      response = await _githubApi.fetchCommitsOfRepo(owner, repository);
+    } catch (err) {
+      throw HttpException();
+    }
+
+    if (response.statusCode != 200) {
+      throw HttpRequestFailure(response.statusCode);
+    }
+
+    try {
+      final body = json.decode(response.body) as List<dynamic>;
+
+      return (body)
+          .map(
+            (commit) => Commit.fromJson(commit),
+          )
+          .toList();
+    } catch (err) {
+      throw JsonDecodeException();
+    }
+  }
+}
--- packages/github_repository/test/github_repository_test.dart
+++ packages/github_repository/test/github_repository_test.dart
@@ -1,4 +1,65 @@
+import 'dart:convert';
+
+import 'package:github_api/github_api.dart';
 import 'package:github_repository/github_repository.dart';
+import 'package:github_repository/src/models/models.dart';
+import 'package:http/http.dart' as http;
+import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
-void main() {}
+class _MockGithubApi extends Mock implements GithubApi {}
+
+void main() {
+  group('Github repository', () {
+    late GithubApi githubApi;
+    late GithubRepository subject2;
+
+    final commitsExpected = List.generate(
+      3,
+      (index) => Commit(
+        committer: Committer(
+          id: index,
+          profileUrl: 'test',
+          avatarUrl: 'test',
+          login: 'test',
+        ),
+        sha: 'test',
+        htmlUrl: 'test',
+        nodeId: 'test',
+      ),
+    );
+
+    setUp(() {
+      githubApi = _MockGithubApi();
+
+      subject2 = GithubRepository(
+        githubApi: githubApi,
+      );
+    });
+
+    group('list of commits in repo:', () {
+      setUp(() {
+        when(() => githubApi.fetchCommitsOfRepo('hydev777', 'committer'))
+            .thenAnswer(
+          (_) async => http.Response(
+            json.encode(
+              commitsExpected
+                  .map(
+                    (commit) => commit.toJson(),
+                  )
+                  .toList(),
+            ),
+            200,
+          ),
+        );
+      });
+
+      test("fetch commits", () async {
+        final listOfCommits =
+            await subject2.fetchListOfCommitsInRepo('hydev777', 'committer');
+
+        expect(listOfCommits, commitsExpected);
+      });
+    });
+  });
+}
