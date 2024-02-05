a0b864e897e2682f91fbf238917378f4570ea5b4
hydev777 <hytechwilson@gmail.com>
2024-02-05 13:54:06 -0400

    clean(pubspec.yaml): removed commented code

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -27,7 +27,6 @@ class App extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         routerConfig: router,
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
       ),
